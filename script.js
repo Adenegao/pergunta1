133245ba47da35811b7b29898e4329b16bdfcd6b
Adenegao <135471605+Adenegao@users.noreply.github.com>
2023-11-20 18:33:39 -0300

    No

--- script.js
+++ script.js
@@ -1,13 +1,29 @@
-<scirpt>
- function verificarResposta(resposta) {
-  var mensagem = document.getElementById('mensagem');
+<script>
+    var jogoAtivo = true;
 
-  if (resposta === 'B') {
-    mensagem.textContent = 'Você acertou! Pode avançar duas casas.';
-    // Lógica para avançar duas casas aqui
-  } else {
-    mensagem.textContent = 'Você errou! Volte cinco casas.';
-    // Lógica para voltar duas casas aqui
-  }
-}
-</script>
+    function verificarResposta(resposta) {
+      if (!jogoAtivo) {
+        return;
+      }
+
+      var mensagem = document.getElementById('mensagem');
+      var btnA = document.getElementById('btnA');
+      var btnB = document.getElementById('btnB');
+      var btnC = document.getElementById('btnC');
+
+      jogoAtivo = false; // Desativa o jogo após a primeira jogada
+
+      if (resposta === 'B') {
+        mensagem.textContent = 'Você acertou! Pode avançar duas casas.';
+        // Lógica para avançar duas casas aqui
+      } else {
+        mensagem.textContent = 'Você errou! Volte cinco casas.';
+        // Lógica para voltar duas casas aqui
+      }
+
+      // Desativa os botões após a jogada
+      btnA.disabled = true;
+      btnB.disabled = true;
+      btnC.disabled = true;
+    }
+  </script>
